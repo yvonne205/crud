bc23b8dd21d4636246b1486ccd8958237f66b0ed
yvonne205 <yvonneabdul205@gmail.com>
2025-01-07 09:17:37 +0000

    defined main function

--- crud.cpp
+++ crud.cpp
@@ -13,3 +13,20 @@ struct Item {
     cout <<"ID:"<<id<<", Name:"<,name<<", Price:$"<<price<<" endl;
 
 }
+
+//function prorotypes//
+void createItems(vector<Item>&items);
+void readItems(const vector<Item>&items);
+void updateItems(vector<Item>&items);
+void deleteItems(vector<Item>&items);
+
+int main () {
+  vector<Item>&items;
+  int choice;
+
+  do{
+  cout<<"/n--- CRUD Menu Operations---/n";
+  cout<<" 1. Create Item/n";
+ 
+  }
+}
